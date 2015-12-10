2e608ce58fd1cb632c412837fb20b65b10fbb553
Dylan Daniels <dylan.david.daniels@gmail.com>
2015-12-09 19:03:42 -0800

    * fixed to sampling code * first test

--- R/ars.R
+++ R/ars.R
@@ -1,8 +1,7 @@
-# abscissae - the x_1 through x_k values already evaluated
 # z - intersection points of envelope function (Note that z[1] may be -Inf, and z[length(z)] may be Inf)
 # u - envelope function (u(Inf) and u(-Inf) must be valid calls, both should return 0)
 # dhx - vector h'(x) evaluted at each of the abscissae
-partialSums <- function (abscissae, z, u, dhx) {
+partialSums <- function (z, u, dhx) {
   lowerZ <- z[-length(z)] # z[1] through z[length(z)-1]
   upperZ <- z[-1] # z[2] through z[length(z)]
   # the evaluated integrals of the envelope function between z[j] and z[j-1] (for each j)
