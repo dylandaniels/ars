1a53775fb91fe4278c8ab5c50754daed37c2a826
Birce Tezel <bircetezel@gmail.com>
2015-12-11 14:46:28 -0800

    Birce commit - Meeting 2

--- R/birce_intersects_acceptReject.R
+++ R/birce_intersects_acceptReject.R
@@ -1,8 +1,8 @@
+setwd("~/Desktop/243Project/")
+
 library(stats)
-library(numDeriv)
 
 #Some trial functions
-
 f <- function(x, lambda = 1)
 {
   if ( x >= 0 )
@@ -25,107 +25,83 @@ df <- function(x, lambda = 1)
   else
   {
     return( 0 )
-  }
+  } 
 }
 
-getC <- function(funcName, min = -Inf, max = Inf)
+
+#Returns TRUE if the vec[j+1] <= vec[j] for all j=1...length(vec)-1
+isDecreasing <- function (vec)
 {
-  c <- tryCatch(integrate(Vectorize(as.function(funcName)), min, max),
-                error = function(e) {
-                  print("Error in integration")
-                  return (0) },
-                warning = function(w) {
-                  print("Warning in integration")
-                  return(0)  })
-  if ( class(c) == "numeric" )
-  {
-    return ( 0 )
-  }
-  else
-  {
-    return (c$value)
-  }
-}
-
-#Given a function f, convertToLog returns
-#the function object of ln f(x)
-convertToLog <- function(f)
-{
-  lnf <- function(x)
-  {
-    return ( log( f(x) ) )
-  }
-  return ( lnf )
+  k <- length(vec)
+  
+  #The differences vec[j+1] - v[j] <= 0 for all j=1...k-1
+  diff <- vec[2:k] - vec[1:k-1]
+  
+  #Count the number of times the difference is <= 0
+  leq <- sum ( diff <= 10e-10 )
+  
+  #If all the differences are <= 0, then the vector is decreasing
+  if ( leq == k-1 )
+    return(TRUE)
+  else #Otherwise, the vector is not decreasing
+    return (FALSE)
 }
 
 
-#Given a function f and df/dx, this function
-#returns the function object of d(ln f)/dx
-convertDerivToLog <- function(f, derivF)
-{
-  derivLogF <- function(x)
-  {
-    return( (1/f(x))*derivF(x) )
-  }
-  return (derivLogF)
-}
-
-#abscissae is the set of points: T_k = { x_1 , x_2, ... , x_k }
-#The function sorts the points in increasing order
+#abscissae is the set of points: T_k = { x_1 , x_2, ... , x_k } (assumption: it is sorted)
 #distFun is g(x), (it is a function object)
 #derivFun is an optional derivative function, i.e. g'(x)
 #If no proof against log-concavity is found, the function returns
 #a list of intersecting points z_j, j=1, ... , k-1
-#Note that the returned vector may be smaller than length k-1 if there are
+#Note that the returned vector may be smaller than length k-1 if there are 
 #abscissae points x_j where h'(x_j) == h'(x_{j+1})
-envelopeIntersectPoints <- function ( abscissae, distFun, derivFun = NULL )
+envelopeIntersectPoints <- function ( abscissae, logDistFun, logDerivFun )
 {
-  #Sort the x_j points in increasing order:
-  abscissae <- sort(abscissae)
-
-  #Take the natural logarithm of the distribution function g(x)
-  #lnDistFun is our h(x)
-  logDistFun <- convertToLog(distFun)
 
   #Evaluate h(x) at the points in abscissae
   hx <- Vectorize(logDistFun)(abscissae)
-
-  #If the user did not provide a derivative function
-  if ( is.null(derivFun) )
-  {
-    #Evaluate h'(x) at the points in abscissae
-    dhx <- grad(Vectorize(logDistFun), abscissae)
-  }
-  else
-  {
-    derivLogFun <- convertDerivToLog(distFun, derivFun)
-    dhx <- Vectorize(derivLogFun)(abscissae)
-  }
-
+  
+  #Evaluate h'(x) at the points in abscissae
+  dhx <- Vectorize(logDerivFun)(abscissae)
+  
   #Check if dhx is decreasing (log-concavity requirement)
-  if( sum( abs( dhx - sort(dhx, decreasing = TRUE) ) ) <= 10e-10 )
+  if( isDecreasing(dhx) )
   {
     #Number of points in abscissae
     k <- length(abscissae)
-
+    
     #Numerator of equation (1)
     numerator <- hx[2:k] - hx[1:k-1] - abscissae[2:k]*dhx[2:k] + abscissae[1:k-1]*dhx[1:k-1]
-
+    
     #Denominator of equation (1)
     denominator <- dhx[1:k-1]-dhx[2:k]
-
+    
     #For points x_j and x_{j+1}, is the derivative the same?
     zeros <- which(denominator <= 10e-10)
-
+    
+    
     #If all the differences are zero, then h(x) must be linear on the abscissae,
-    #and there are no intersecting points, the function returns x_1
+    #and there are no intersecting points, the function returns x_1, x_k
+    
+    #Initialize the intersects vector
+    intersects <- rep(0, k-1)
     if ( length(zeros) == k-1 )
     {
-      intersects <- c(min(abscissae), max(abscissae))
+      intersects <- c( rep(min(abscissae), k-2), max(abscissae) )
     }
     else if (length(zeros) != 0) #If some of the differences are equal to zero
     {
-      intersects <- numerator[-zeros]/denominator[-zeros]
+      #For the points where the denominator is not equal to zero
+      intersects[-zeros] <- numerator[-zeros]/denominator[-zeros]
+      
+      for (i in 1:length(zeros))
+      {
+        index <- zeros[i]
+        if (index != 0)
+          intersects[index] <- intersects[index-1]
+        else
+          intersects[index] <- min(abscissae)
+      }
     }
     else #If none of the differences are equal to zero.
     {
@@ -136,12 +112,52 @@ envelopeIntersectPoints <- function ( abscissae, distFun, derivFun = NULL )
   else #If the h'(x) is not decreasing
   {
     warning ("The sampling function is not log-concave.")
-    print(dhx)
-    print(sort(dhx, decreasing = TRUE) )
-    print(sum( abs( dhx - sort(dhx, decreasing = TRUE) ) ))
     return (NULL)
   }
 }
+
+#NOT FINISHED YET
+updateDistVals <- function(abscissae, hx, dhx, xStar, logDistFunc, logDerivFun)
+{
+  leq <- (abscissae <= xStar)
+  index <- sum(leq) + 1
+  
+  k <- length(abscissae)
+  newHx <- rep(0, k+1)
+  newDhx <- rep(0, k+1)
+  
+  newHx <- c(hx[1:index], logDistFunc(xStar) , hx[(index+1):k])
+  newDhx <- c(dhx[1:index], logDerivFun(xStar) , dhx[(index+1):k])
+  
+  #Check if the dhx vector is still decreasing
+  if ( (newDhx[index+1] - newDhx[index] > 10e-10) || (newDhx[index+2] - newDhx[index+1] > 10e-10) )
+    warning("In updateDistVals: Log-concavity assumption is violated, the vector of h'(x) is non-decreasing.")
+  
+  return( list(hx = newHx, dhx = newDhx) )
+}
+
+updateIntersects <- function(abscissae, intersects, xStar, hx, dhx)
+{
+  leq <- (abscissae <= xStar)
+  index <- sum(leq) + 1
+  
+  k <- length(abscissae)
+  
+  newIntersects <- rep(0, k)
+  newIntersects[1:(index-1)] <- intersects[1:index-1]
+  
+  #Intersection of the tangent lines at x_index and xStar
+  xj <- 
+  newIntersects[index] <- hx[index] - hx[index+1] - abscissae[index]*dhx[index] - 
+    
+  #Intersection of the tangent lines at xStar and x_{index+1}
+  newIntersects[index + 1] <- smthng
+  newIntersects[(index+2):k] <- intersects[(index+1):k-1]
+  
+  
+  
+}
+
 
 #xStar is a point sampled from s_k(x) function
 #lowerFun is the l_k(x) function
@@ -153,16 +169,22 @@ acceptReject <- function (xStar, lowerFun, upperFun, logDistFun)
 {
   #A random uniform(0,1) value
   w <- runif(1)
-  accepted <- FALSE
 
-  if ( w <= exp( lowerFun(xStar) - upperFun(xStar) ) ) #Squeezing test
-  {
-    accepted <- TRUE
-  }
-  else if ( w <= exp ( logDistFun(xStar) - upperFun(xStar) ) ) #Rejection test
-  {
-    accepted <- TRUE
-  }
+  myList <- list(dec = FALSE, step = NULL, hx = NULL, dhx = NULL)
 
-  return (accepted)
+  upperVal <- upperFun(xStar)
+  if ( w <= exp( lowerFun(xStar) - upperVal ) ) #Squeezing test
+  {
+    myList$dec <- TRUE
+    myList$step <- 1
+  }
+  else if ( w <= exp ( logDistFun(xStar) - upperVal ) ) #Rejection test
+  {
+    myList$dec <- TRUE
+    myList$step <- 2
+    myList$hx <- logDistFun(xStar)
+    myList$dhx <- logDistDerivFun(xStar)
+  }
+  
+  return ( myList )
 }
