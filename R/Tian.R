8d4525da18eef4cf5ab6987760e9ce0e5e84e767
Tian Xia <txia@berkeley.edu>
2015-12-10 22:43:48 -0800

    first change

--- R/Tian.R
+++ R/Tian.R
@@ -4,30 +4,58 @@
 # sampleFunc is h(x)
 # derivSampleFunc is h'(x)
 
-
-#I'm not sure what the output should be at this moment
-envelope <- function(x,z,sampleFunc){
+#take the input vector of z(sorted), one value of xstar, vector of h(x) and vector of h'(x)
+#and output the value of u_k(xstar)
+envelope <- function(z,xstar,sampleFunc,derivX){
+  #calculate the vector of absolute difference between z and xstar
+  diffs <- abs(xstar-z)
+  #calcuate the index of the min of the differences
+  index <- which.min(diffs)
   
-  x <- as.numeric(sort(x))
-  derivX <- numericDeriv(quote(sampleFunc),'x')
-  dX <- diag(attr(derivX,'gradient'))
-  dimZ <- length(z)
-  z <- sort(z)
-  u <- vector('list',dimZ)
-  
-  for (i in 0:(dimZ-1)){
-    u[[i+1]] <- function(x)sampleFunc(x[i+1])+(quote(x)-x[i+1])*dX[i+1]
+  #for the z_index which has the min absolute difference with xstar,
+  #either it's the upper bound or lower bound of the interval in which
+  #xstar falls
+  if(xstar>=z[index-1] && xstar<=z[index]){
+    ustar <- sampleFunc[index]+(xstar-z[index])*derivX[index]
+  }else if (xstar>z[index] && xstar<=z[index+1]){
+    ustar <- sampleFunc[index+1]+(xstar-z[index+1])*derivX[index+1]
   }
-  return(u)
+  
+  #return ustar
+  return(ustar)
 }
 
+#to test the function
+z <- rnorm(10)
+z <- sort(z)
+minZ <- min(z)
+maxZ <- max(z)
+xstar <- runif(1,minZ,maxZ)
+sampleFunc <- rnorm(10)
+derivX <- rnorm(10)
 
-##need to modify later
-updateStep <- function(z,xstar){
-  z <- c(z,xstar)
-  z <- sort(z)
-  #call u_k+1(x)
-  #call s_k+1(x)
-  #call l_k+1(x)
+envelope(z,xstar,sampleFunc,derivX)
+
+##
+#take the vector of z, current value of xstar, and the result of rejection-or-not
+updateStep <- function(z,xstar,result,x,hx,dx){
+  #if the result is true, add xstar to T_x, and update z
+  if (result=TRUE){
+    #add xstar to T_x and sort
+    x <- c(x,xstar)
+    x<- sort(x)
+    
+    #update z
+    z <- envelopeIntersectPoints(x,distFun,derivFun = NULL)
+    
+    ##update hx and dx,maybe we can use envelopeIntersectPoints fucntion
+    ##to also output hx and dx?
+    hx <- hx(x)
+    dx <- dx(x)
+  }
+  
   
 }
+
+
+
