0f5955210c19bcb8e2032ce8cfc2a6baeabfe14e
Tian Xia <txia@berkeley.edu>
2015-12-11 14:46:51 -0800

    second change

--- R/Tian.R
+++ R/Tian.R
@@ -6,7 +6,7 @@
 
 #take the input vector of z(sorted), one value of xstar, vector of h(x) and vector of h'(x)
 #and output the value of u_k(xstar)
-envelope <- function(z,xstar,sampleFunc,derivX){
+envelope <- function(z,x,xstar,sampleFunc,derivX){
   #calculate the vector of absolute difference between z and xstar
   diffs <- abs(xstar-z)
   #calcuate the index of the min of the differences
@@ -15,26 +15,50 @@ envelope <- function(z,xstar,sampleFunc,derivX){
   #for the z_index which has the min absolute difference with xstar,
   #either it's the upper bound or lower bound of the interval in which
   #xstar falls
-  if(xstar>=z[index-1] && xstar<=z[index]){
-    ustar <- sampleFunc[index]+(xstar-z[index])*derivX[index]
-  }else if (xstar>z[index] && xstar<=z[index+1]){
-    ustar <- sampleFunc[index+1]+(xstar-z[index+1])*derivX[index+1]
+  if (index==1){
+    #if xstar is closest to z[1],we have two cases
+    if(xstar<=z[1]){
+      ustar <- sampleFunc[1]+(xstar-x[1])*derivX[1]
+    }else{
+      ustar <- sampleFunc[2]+(xstar-x[2])*derivX[2]
+    }
+  } else{
+    if(xstar>=z[index-1] && xstar<=z[index]){
+      ustar <- sampleFunc[index]+(xstar-x[index])*derivX[index]
+    }else if (xstar>z[index] && xstar<=z[index+1]){
+      ustar <- sampleFunc[index+1]+(xstar-x[index+1])*derivX[index+1]
+    }
+    
   }
+  
   
   #return ustar
   return(ustar)
 }
 
 #to test the function
-z <- rnorm(10)
-z <- sort(z)
-minZ <- min(z)
-maxZ <- max(z)
-xstar <- runif(1,minZ,maxZ)
-sampleFunc <- rnorm(10)
-derivX <- rnorm(10)
+##testtaht
+context('envelope returns the correct u_k(xstar)')
 
-envelope(z,xstar,sampleFunc,derivX)
+test_that('envelope() returns the correct output', {
+  
+  z <- c(1,2,3)
+  x <- c(.5,1.5,2.5)
+  xstar <- 0.4
+  sampleFunc <- c(10,20,8)
+  derivX <- c(1,.8,.6)
+  expect_equal(envelope(z, x,xstar,sampleFunc,derivX), 9.9)
+})
+
+test_that('envelope() returns the correct output', {
+  
+  z <- c(1,2,3)
+  x <- c(.5,1.5,2.5)
+  xstar <- 2.6
+  sampleFunc <- c(10,20,8)
+  derivX <- c(1,.8,.6)
+  expect_equal(envelope(z, x,xstar,sampleFunc,derivX), 8.06)
+})
 
 ##
 #take the vector of z, current value of xstar, and the result of rejection-or-not
