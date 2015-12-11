ab3f5aa808446f70df4f49be12d9da6c948cb52f
Dylan Daniels <dylan.david.daniels@gmail.com>
2015-12-11 15:07:41 -0800

    Update Mainline.R
    fixes to mainline

--- R/Mainline.R
+++ R/Mainline.R
@@ -1,17 +1,18 @@
-Mainline=function(g,x,leftbound=-Inf,rightbound=Inf)
-{ y=x
-  h=function(y){
-    return (g(y))
+Mainline <- function(g,abscissae,leftbound=-Inf,rightbound=Inf) { 
+  h <- function(y) {
+    return (log(g(y)))
   }
-  h_der=function(h,x){
+  h_der=function(x){
     y=x
     dh_x=diag(attributes(numericDeriv(quote(h(y)),'y'))$gradien)
 return(dh_x)
   }
 
-abscissae=ininitialize()
-z=envelopeIntersectionPoints(x, z, abscissae, h)
-u=envelope (x, z, abscissae, h)
+#abscissae=ininitialize() # or get abscissae from user input
+hx = h(abscissae)
+dhx = h_der(abscissae)
+z=envelopeIntersectionPoints(abscissae, hx, dhx)
+u=envelope (z, x, xstar, hx, dhx)
 s=normalizedEnvelope(x, z, abscissae, h)
 l=squeezing(h,abscissae,x)
 sample=sampleFromEnvelope(abscissae, z, u, h,x)
