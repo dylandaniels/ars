0257fd50fec1d85a7aecf266e39685ea1acf5cae
Oski Bear <oski@bear>
2015-12-12 14:30:38 -0800

    add squeezing.R

---
+++ R/squeezing.R
@@ -0,0 +1,20 @@
+#***********document*********************************************************
+#we import h(x),abscissae,and x value And returns l(x)(which is squeezing here),after that if we use exp(l(x)),we get T(x)
+#*****************************************************************************
+squeezing=function(h,abscissae,x){
+  k=length(abscissae)
+  if(x<abscissae[1] || x>abscissae[k])
+  {return (-Inf)}
+   for(i in 1:(k-1))
+    {
+      if(x>=abscissae[i] && x<=abscissae[i+1])
+    {
+      return ((abscissae[i+1]-x)*h(abscissae[i])+(x-abscissae[i])*h(abscissae[i+1])/(abscissae[i+1]-abscissae[i]))
+    }
+    }
+    }
+
+
+
+
+
