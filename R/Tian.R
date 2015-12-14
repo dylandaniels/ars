f436f7267a68731e09992af5da3023198bbfa243
Tian Xia <txia@berkeley.edu>
2015-12-14 09:41:46 -0800

    add comment and test function

--- R/Tian.R
+++ R/Tian.R
@@ -1,50 +1,68 @@
-# This is equation (2) on page 340
-# x is the point to evaluate
-# z is the vector of envelop intersection points
-# sampleFunc is h(x)
-# derivSampleFunc is h'(x)
 
-#take the input vector of z(sorted), one value of xstar, vector of h(x) and vector of h'(x)
-#and output the value of u_k(xstar)
-# TODO: rename x to abscissae
-# TODO: rename xstar to x
+#this 'envelop' function works to output the value of u_k(x), which is the upper hull function when exponentiated and
+#normalized. This function takes inputs of z(the intersection points), abscissae, the x value to be evaluated, hx(the
+#log function value of the abscissae) and dhx(derivative values of log function).
+
 envelope <- function(z, abscissae, x, hx, dhx) {
-  #print(paste0('x=',x))
-  #print(paste0('z=',z))
-
+  #Here, 'index' determines which piecewise function will be used to evaluate u_k(x)
+  #if the 'x' equals to last element of 'z', then the index will be length of 'z' minus
+  #1,otherwise it will be the the sum of how many items in z are less than or equal to
+  #'x'.
   if (x == z[length(z)]) {
     index = length(z) - 1
   } else {
     index <- sum(z <= x)
   }
+
+  #plug in the value, compute u(x) and return the value.
   ux <- hx[index] + (x - abscissae[index]) * dhx[index]
   return(ux)
 }
 
 #to test the function
-##testtaht
-# context('envelope returns the correct u_k(xstar)')
-#
-# test_that('envelope() returns the correct output', {
-#
-#   z <- c(1,2,3)
-#   x <- c(.5,1.5,2.5)
-#   xstar <- 0.4
-#   sampleFunc <- c(10,20,8)
-#   derivX <- c(1,.8,.6)
-#   expect_equal(envelope(z, x,xstar,sampleFunc,derivX), 9.9)
-# })
-#
-# test_that('envelope() returns the correct output', {
-#
-#   z <- c(1,2,3)
-#   x <- c(.5,1.5,2.5)
-#   xstar <- 2.6
-#   sampleFunc <- c(10,20,8)
-#   derivX <- c(1,.8,.6)
-#   expect_equal(envelope(z, x,xstar,sampleFunc,derivX), 8.06)
-# })
+library(testthat)
+context('envelope returns the correct u_k(x)')
 
+#Test the regular cases
+test_that('envelope() returns the correct output', {
 
+  z <- c(0,1,2,3)
+  abscissae<- c(.5,1.5,2.5)
+  x<- 0.4
+  hx <- c(10,20,8)
+  dhx <- c(1,.8,.6)
+  expect_equal(envelope(z, abscissae, x,hx,dhx), 9.9)
+})
+
+test_that('envelope() returns the correct output', {
+
+  z <- c(0,1,2,3)
+  abscissae <- c(.5,1.5,2.5)
+  x <- 2.6
+  hx <- c(10,20,8)
+  dhx <- c(1,.8,.6)
+  expect_equal(envelope(z, abscissae, x, hx, dhx) , 8.06)
+})
+
+#Test the corner case where 'x' equals to the upper bound and lower bound
+test_that('envelope() returns the correct output', {
+
+  z <- c(0,1,2,3)
+  abscissae<- c(.5,1.5,2.5)
+  x<- 3
+  hx <- c(10,20,8)
+  dhx <- c(1,.8,.6)
+  expect_equal(envelope(z, abscissae, x,hx,dhx), 8.3)
+})
+
+test_that('envelope() returns the correct output', {
+
+  z <- c(0,1,2,3)
+  abscissae<- c(.5,1.5,2.5)
+  x<- 0
+  hx <- c(10,20,8)
+  dhx <- c(1,.8,.6)
+  expect_equal(envelope(z, abscissae, x,hx,dhx), 9.5)
+})
 
 
