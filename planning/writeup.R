1942d72fe89b911a57d5aeaaadce4bdd5bdff326
Birce Tezel <bircetezel@gmail.com>
2015-12-17 13:40:13 -0800

    some formatting on the histograms

--- planning/writeup.R
+++ planning/writeup.R
@@ -4,10 +4,12 @@ ddgamma=Vectorize(function(x,alpha=4,beta=5)
 {
   return(dgamma(x,shape=alpha,rate=beta))
 })
+
 ddbeta=Vectorize(function(x,alpha=2,beta=2)
 {
   return(dbeta(x,shape1=alpha,shape2=beta))
 })
+
 ddchisqu=Vectorize(function(x,k=6)
 {
   return(1/(2^(k/2)*gamma(k/2))*x^(k/2-1)*exp(-x/2))
@@ -16,7 +18,8 @@ ddchisqu=Vectorize(function(x,k=6)
 ppgamma=Vectorize(function(x,alpha=4,beta=5)
 {
   return(pgamma(x,shape=alpha,rate=beta))
-}
+})
+
 ppbeta=Vectorize(function(x,alpha=2,beta=2)
 {
   return(pbeta(x,shape1=alpha,shape2=beta))
@@ -25,23 +28,22 @@ ppbeta=Vectorize(function(x,alpha=2,beta=2)
 ddchisq=Vectorize(function(x,df=6)
 {
   return(dchisq(x,df))
-}
-)
+})
+
 ppchisq=Vectorize(function(x,df=6)
 {
   return(pchisq(x,df))
-}
-)
+})
 
 
 
 
 set.seed(0)
 samples=ars(n=1000, g=dnorm, dg=NULL, initialPoints=NULL, leftbound=-Inf, rightbound=Inf)
-png(filename="the_distribution_of_samples_vs_normal_distribution.png")
+png(filename="standard_normal.png")
 hist(samples,probability=TRUE,main=NULL)
-plot(dnorm,xlim=c(-3,3),add=TRUE,col="blue",main='the distribution of samples vs normal distribution')
-title(main='the distribution of samples vs normal(0,1) distribution')
+plot(dnorm,xlim=c(-3,3),add=TRUE,col="blue",main='Standard Normal')
+title(main='Standard Normal')
 dev.off()
 ks.test(samples,pnorm)$p.value
 #From the figure, we can see that the samples approximately follow normal(0,1) distribution.
@@ -50,10 +52,10 @@ ks.test(samples,pnorm)$p.value
 
 set.seed(0)
 samples=ars(1000, g=dunif,leftbound=0, rightbound=1)
-png(filename="the_distribution_of_samples_vs_uniform(0,1)_distribution.png")
+png(filename="uniform.png")
 hist(samples,probability=TRUE,main=NULL)
-plot(dunif,xlim=c(0,1),add=TRUE,col="blue",main='the distribution of samples vs uniform distribution')
-title(main='the distribution of samples vs uniform(0,1) distribution')
+plot(dunif,xlim=c(0,1),add=TRUE,col="blue",main='Uniform(0,1)')
+title(main='Uniform(0,1)')
 dev.off()
 ks.test(samples,punif)$p.value
 #From the figure, we can see that the samples approximately follow uniform(0,1) distribution.
@@ -62,10 +64,10 @@ ks.test(samples,punif)$p.value
 
 set.seed(0)
 samples=ars(1000, g=ddgamma,leftbound=0, rightbound=Inf)
-png(filename="the_distribution_of_samples_vs_gamma(4,5)_distribution.png")
+png(filename="gamma.png")
 hist(samples,probability=TRUE,main=NULL)
-plot(ddgamma,xlim=c(0,10),add=TRUE,col="blue",main='the distribution of samples vs gamma(4,5) distribution')
-title(main='the distribution of samples vs gamma(4,5) distribution')
+plot(ddgamma,xlim=c(0,10),add=TRUE,col="blue",main='Gamma(4,5)')
+title(main='Gamma(4,5)')
 dev.off()
 ks.test(samples,ppgamma)$p.value
 #From the figure, we can see that the samples approximately follow gammma(4,5) distribution.
@@ -75,10 +77,10 @@ ks.test(samples,ppgamma)$p.value
 
 set.seed(0)
 samples=ars(1000, g=ddbeta,leftbound=0, rightbound=1)
-png(filename="the_distribution_of_samples_vs_beta(2,2)_distribution.png")
+png(filename="beta.png")
 hist(samples,probability=TRUE,main=NULL)
-plot(ddbeta,xlim=c(0,10),add=TRUE,col="blue",main='the distribution of samples vs beta(2,2) distribution')
-title(main='the distribution of samples vs beta(2,2) distribution')
+plot(ddbeta,xlim=c(0,10),add=TRUE,col="blue",main='Beta(2,2)')
+title(main='Beta(2,2)')
 dev.off()
 ks.test(samples,ppbeta)$p.value
 #From the figure, we can see that the samples approximately follow beta(2,2) distribution.
@@ -88,10 +90,10 @@ ks.test(samples,ppbeta)$p.value
 
 set.seed(0)
 samples=ars(1000, g=ddchisq,leftbound=0, rightbound=Inf)
-png(filename="the_distribution_of_samples_vs_chisq(6)_distribution.png")
+png(filename="chisq.png")
 hist(samples,probability=TRUE,main=NULL)
-plot(ddchisqu,xlim=c(0,20),add=TRUE,col="blue",main='the distribution of samples vs chisq(6) distribution')
-title(main='the distribution of samples vs chisq(6) distribution')
+plot(ddchisqu,xlim=c(0,20),add=TRUE,col="blue",main='Chi-squared(4)')
+title(main='Chi-squared(4)')
 dev.off()
 ks.test(samples,ppchisq)$p.value
 #From the figure, we can see that the samples approximately follow chisq(6) distribution.
